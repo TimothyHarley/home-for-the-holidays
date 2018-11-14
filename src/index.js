0f282ef57e13ef892fba2fff4651d21568d04203
Timothy Harley <timothy.d.harley@gmail.com>
2018-11-13 21:23:02 -0600

    buttons and divs only display when they should

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import firebase from 'firebase/app';
 import createNavbar from './components/navbar';
 import apiKeys from '../db/apiKeys.json';
 import loginButton from './Auth/auth';
+import checkLoginStatus from './helpers/authHelpers';
 
 const initApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
+  checkLoginStatus();
   loginButton();
 };
 
