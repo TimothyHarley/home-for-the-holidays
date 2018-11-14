20cacb2596fbaa2a3fc1338ce619f8145a59f00f
Timothy Harley <timothy.d.harley@gmail.com>
2018-11-13 20:50:45 -0600

    I can log out now

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,21 @@
 import './navbar.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import $ from 'jquery';
+
+const navbarEvents = () => {
+  $('.nav-link').on('click', (e) => {
+    if (e.target.id === 'navbar-button-logout') {
+      firebase.auth().signOut().then(() => {
+        console.log('you logged out');
+      }).catch((err) => {
+        console.error('you are still logged in', err);
+      });
+    } else {
+      console.log(e.target.id);
+    }
+  });
+};
 
 const createNavbar = () => {
   const domString = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -27,6 +43,7 @@ const createNavbar = () => {
 </nav>`;
 
   $('#navbar').html(domString);
+  navbarEvents();
 };
 
 export default createNavbar;
