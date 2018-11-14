054f473911eef9d07dbfebbd7a2af0a4323cbe7c
Timothy Harley <timothy.d.harley@gmail.com>
2018-11-13 20:32:27 -0600

    login styled

--- src/Auth/auth.js
+++ src/Auth/auth.js
@@ -1,12 +1,15 @@
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import googleImage from './sign-in.png';
 import './auth.scss';
 
 
 const loginButton = () => {
   const domString = `
-    <button id='google-auth' class="btn btn-secondary">Login</button>
+    <button id='google-auth' class="btn">
+      <img src='${googleImage}'/>
+    </button>
   `;
   $('#auth').html(domString);
   $('#google-auth').on('click', () => {
