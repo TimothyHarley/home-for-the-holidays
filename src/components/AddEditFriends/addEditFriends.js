91b3ed08420488d52b18374b585367e96a677756
Timothy Harley <timothy.d.harley@gmail.com>
2018-11-19 21:25:54 -0600

    wip

---
+++ src/components/AddEditFriends/addEditFriends.js
@@ -0,0 +1,45 @@
+import $ from 'jquery';
+import authHelpers from '../../helpers/authHelpers';
+
+const formBuilder = () => {
+  const form = `
+  <div class="form-group">
+    <label for="form-friend-name">Name:</label>
+    <input type="text" class="form-control" id="form-friend-name" placeholder="John Smith">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-address">Address:</label>
+    <input type="text" class="form-control" id="form-friend-address" placeholder="500 Interstate Avn">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-email">Email:</label>
+    <input type="text" class="form-control" id="form-friend-email" placeholder="fake@email.com">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-phoneNumber">Phone Number:</label>
+    <input type="text" class="form-control" id="form-friend-phoneNumber" placeholder="555-555-5555">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-relationship">Relationship:</label>
+    <input type="text" class="form-control" id="form-friend-relationship" placeholder="Frienemy">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  `;
+  return form;
+};
+
+const gettingFriendFromForm = () => {
+  const friend = {
+    name: $('#form-friend-name').val(),
+    address: $('#form-friend-address').val(),
+    email: $('#form-friend-email').val(),
+    phoneNumber: $('#form-friend-phoneNumber').val(),
+    relationship: $('#form-friend-relationship').val(),
+    isAvoiding: false,
+    uid: authHelpers.getCurrentUid()
+  };
+};
